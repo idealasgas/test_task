95bc561a7d16548ba3ea3337e5ff946422b6145f
Marie Akhramenko <triangletrue@yahoo.com>
2020-04-09 18:46:14 +0300

    Add behaviour to calculator_spec

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,7 +1,7 @@
 require 'calculator'
 
 RSpec.describe Calculator do
-  let(:incoming_hash) {{'/help_page/1': ['126.318.035.038', '136.343.035.787']}}
+  let(:incoming_hash) {{'/help_page/1': ['126.318.035.038', '126.318.035.038']}}
   subject {Calculator.new(incoming_hash)}
 
   it 'has calculate_views method' do
@@ -10,5 +10,25 @@ RSpec.describe Calculator do
 
   it 'has calculate_unique_views method' do
     expect(subject).to respond_to(:calculate_unique_views)
+  end
+
+  describe '#calculate_views' do
+    it 'returns hash with values of integer' do
+      result = subject.calculate_views
+
+      expect(result).to be_kind_of(Hash)
+      expect(result.values.first).to be_kind_of(Integer)
+      expect(result.values.first).to eq 2
+    end
+  end
+
+  describe '#calculate_unique_views' do
+    it 'returns hash with values of integer' do
+      result = subject.calculate_unique_views
+
+      expect(result).to be_kind_of(Hash)
+      expect(result.values.first).to be_kind_of(Integer)
+      expect(result.values.first).to eq 1
+    end
   end
 end
