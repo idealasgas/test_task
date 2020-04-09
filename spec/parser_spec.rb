3135ff6049733a7b59c60e478c621e2ff86dd6fe
Marie Akhramenko <triangletrue@yahoo.com>
2020-04-09 18:07:37 +0300

    Test for behaviour of parser

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,7 +1,26 @@
 require 'parser'
 
 describe Parser do
+  let (:existing_file) {'webserver.log'}
+  let (:non_existing_file) {'i_do_not_exist.log'}
+
   it 'should respond to #parse method' do
-    expect(subject).to respond_to(:parse)
+    parser = described_class.new(existing_file)
+    expect(parser).to respond_to(:parse)
+  end
+
+  describe '#parse' do
+    it 'should raise error if provided file not found' do
+      parser = described_class.new(non_existing_file)
+
+      expect { parser.parse }.to raise_error('FileNotFound')
+    end
+
+    it 'should return parsed hash' do
+      parser = described_class.new(existing_file)
+
+      expect(parser.parse).to be_kind_of(Hash)
+      expect(parser.parse).not_to be_empty
+    end
   end
 end
